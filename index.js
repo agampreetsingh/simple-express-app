863dec7714b96fa30c8504bf527f90705d7bc8da
agampreetsingh <agampreet_singh@yahoo.com>
2018-04-12 01:49:37 +0530

    Completed

--- index.js
+++ index.js
@@ -2,40 +2,50 @@ const express = require('express')
 
 const app = express()
 
-let arr=["Hello", "Bye"];
+arr=['dasdas','hello','bye'];
 
 app.use(function (req, res, next) {
     console.log("Server Running on port: 8181")
     next()
 })
 app.get('/todos', (req, res) => {
-             res.send(arr);
+   
+    let responseList='<ul>';
+    for(let i=0;i<arr.length;i++)
+    {
+    
+        responseList+='<li>'+arr[i]+'</li>';
+    }        
+    responseList+='</ul>';
+    
+    res.send(responseList);
     
 })
 app.get('/todos/add', (req, res) => {
    
     arr.push(req.query.task)
     res.send("added")
+
 })
 
 app.get('/todos/delete', (req, res) => {
   
-    res.send("a")
+    arr.splice(req.query.id,1);
+    res.send("deleted")
+
 })
 
-app.get('/a/b', (req, res) => {
-    console.log("You requested a/b")
-    res.send("a/b")
+app.get('/todos/update', (req, res) => {
+  
+    arr.splice(req.query.id,1,req.query.task);
+    res.send("updated")
+
 })
 
-app.get('/b/a', (req, res) => {
-    console.log("You requested b/a")
-    res.send("b/a")
-})
 
 app.use(function (req, res) {
     res.status(404).send("<h1>No such page found</h1>")
 })
 
-
+sa
 app.listen(8181)
